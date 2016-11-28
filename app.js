28443e15056f4d57fec9a9118ccda2feda0615cb
Adam <musician903@gmail.com>
2016-11-28 15:06:53 -0600

    Changed console.log in POST

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.get('/login',
 app.post('/login/callback',
     passport.authenticate('saml', { failureRedirect: '/login/fail' }),
     function (req, res) {
-        console.log(req["issuer"]);
+        console.log(json.stringify(req)["issuer"]);
         res.redirect('/');
     }
 );
