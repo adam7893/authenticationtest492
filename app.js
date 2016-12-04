b3b5056c30a23bf23b292167a305c71aad5d2938
Adam <musician903@gmail.com>
2016-12-04 14:03:26 -0600

    expanded comment in serializeUser()

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var usersaml;
 
 passport.serializeUser(function (user, done) {
     console.log(user['issuer']['_']);
-    console.log(user['issuer']['$']);
+    console.log(user['issuer']['$']['Format']);
     usersaml = {};
     //usersaml.nameID = profile.nameID;
     //usersaml.nameIDFormat = profile.nameIDFormat;
