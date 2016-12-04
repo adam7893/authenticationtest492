94fdf96cf70090afb97d5746b5689774c3447670
Adam <musician903@gmail.com>
2016-12-04 13:06:52 -0600

    added logout within destroy

--- app.js
+++ app.js
@@ -178,6 +178,7 @@ app.get('/logout', function (req, res) {
     res.redirect('https://testshib.org/Shibboleth.sso/Logout');*/
 
     req.session.destroy( function() {
+        req.logout();
         res.redirect('https://testshib.org/Shibboleth.sso/Logout');
     });
 
