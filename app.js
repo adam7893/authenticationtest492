d19e7ea7d7f652b6a0545c09c5ad5d3cd6e85681
Adam <musician903@gmail.com>
2016-12-04 14:07:28 -0600

    added JSON.stringify()

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var usersaml;
 
 passport.serializeUser(function (user, done) {
     console.log(user['issuer']['_']);
-    console.log(user['issuer']['$']);
+    console.log(JSON.stringify(user['issuer']['$']));
     usersaml = {};
     //usersaml.nameID = profile.nameID;
     //usersaml.nameIDFormat = profile.nameIDFormat;
