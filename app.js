8d978d044936e02cb6443d9d14c16a2597036831
Adam <musician903@gmail.com>
2017-01-28 16:47:30 -0600

    trying passport.logoutSaml again...

--- app.js
+++ app.js
@@ -216,6 +216,10 @@ app.post("/logout", function (req, res) {
     }
 });
 
+app.get('/logout', function (req, res) {
+    passport.logoutSaml(req, res);
+})
+
 passport.logoutSaml = function (req, res) {
     if (usersaml != null) {
         //Here add the nameID and nameIDFormat to the user if you stored it someplace.
