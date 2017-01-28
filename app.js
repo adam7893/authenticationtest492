d5e07584f908015dee83be2d36bf508432ee23d2
Adam <musician903@gmail.com>
2017-01-28 16:20:03 -0600

    reverted commenting out of app.js

--- app.js
+++ app.js
@@ -51,12 +51,10 @@ var samlStrategy = new saml.Strategy({
     privateCert: process.env.KEY,
     // Identity Provider's public key
     cert: fs.readFileSync(__dirname + '/cert/idp_cert.pem', 'utf8'),
-    /*
     validateInResponseTo: false,
     disableRequestedAuthnContext: true,
     forceAuthn: true,
     isPassive: false,
-    */
     additionalParams: {}
 }, function (profile, done) {
     return done(null, profile);
