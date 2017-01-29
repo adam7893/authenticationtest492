6c4fb4de15db3d0861909dc92d5061da7c9ee42a
Adam <musician903@gmail.com>
2017-01-28 18:45:36 -0600

    forceAuthn: true

--- app.js
+++ app.js
@@ -53,9 +53,7 @@ var samlStrategy = new saml.Strategy({
     cert: fs.readFileSync(__dirname + '/cert/idp_cert.pem', 'utf8'),
     validateInResponseTo: false,
     disableRequestedAuthnContext: true,
-    /*
     forceAuthn: true,
-    */
     isPassive: false,
     additionalParams: {}
 }, function (profile, done) {
