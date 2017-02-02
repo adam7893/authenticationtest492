8caa849abd4bb87cfe25f6ee3a9b8b34cf5dac96
Adam <musician903@gmail.com>
2017-02-01 19:40:19 -0600

    req, res clear cookie

--- app.js
+++ app.js
@@ -159,6 +159,8 @@ var parameters = {
                     //redirect to the IdP Logout URL
                     console.log("Redirecting to " + request);
                     req.session.destroy(function (err) {
+                        req.clearCookies('connect.sid');
+                        res.clearCookies('connect-sid');
                         req.logout();
                         res.redirect(request);
                     });
